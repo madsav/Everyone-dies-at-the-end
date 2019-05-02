16f538652c21cba35a6c941c5c1501a9efad2631
Madara <savicka.madara@gmail.com>
2019-05-03 02:37:25 +0300

    ..

--- Assets/Korutina/Coroutine.cs
+++ Assets/Korutina/Coroutine.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class Coroutine : MonoBehaviour
 {
     [SerializeField] private GameObject Vase2;
-    
-   
+
+    [SerializeField] private GameObject Column1;
+
+    private static int score;
+
     private void Start()
     {
         
         StartCoroutine(HideObject());
-    
+        Update();
+      
+    }
+
+   void Update()
+    {
+        score = Wolf_Movement.count;
+
+
+        if (score == 6)
+        {
+            Column1.SetActive(false);
+        }
     }
 
     IEnumerator HideObject()
     {
-    
         yield return new WaitForSeconds(5);
         Vase2.SetActive(false);
 
+       
+       
     }
+
 }
